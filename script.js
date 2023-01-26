5de1b43a436d0d425a0ca9e9bd53ad5e9ebe1275
Fábio Bronze <fabio.r.bronze@hotmail.com>
2023-01-26 02:23:59 +0000

    update

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 "use strict";
+
+const el = document.querySelector(".menu-mobile-icon");
+
+el.addEventListener("click", () => {
+  const menuItems = document.querySelector(".menu-items");
+  if (menuItems.classList.contains("show")) {
+    menuItems.classList.add("hide");
+    menuItems.classList.remove("show");
+  } else {
+    menuItems.classList.add("show");
+    menuItems.classList.remove("hide");
+  }
+});
